89aee70d0469944a493b2b773727d28c01d07360
Rafles29 <rwozniak@intelgraf.com.pl>
2017-08-22 13:46:39 +0200

    4 week assigment

---
+++ conFusion/scripts/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#carousel-button").click(function(){
+                 if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+                     $("#mycarousel").carousel('pause');
+                     $("#carousel-button").children("span").removeClass('fa-pause');
+                     $("#carousel-button").children("span").addClass('fa-play');
+                 }
+                 else if ($("#carousel-button").children("span").hasClass('fa-play')){
+                     $("#mycarousel").carousel('cycle');
+                     $("#carousel-button").children("span").removeClass('fa-play');
+                     $("#carousel-button").children("span").addClass('fa-pause');
+                 }
+             });
+  $("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+  });
+  $("#reserveButton").click(function () {
+    $("#resModal").modal("show");
+  });
+});
