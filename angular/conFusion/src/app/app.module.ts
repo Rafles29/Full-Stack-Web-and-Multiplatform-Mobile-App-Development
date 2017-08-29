d2ad5c94732a4ab61a0a5507232131d293ccb5bd
Rafles29 <rwozniak@intelgraf.com.pl>
2017-08-29 13:42:34 +0200

    minnor change

--- angular/conFusion/src/app/app.module.ts
+++ angular/conFusion/src/app/app.module.ts
@@ -6,15 +6,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import 'hammerjs';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
