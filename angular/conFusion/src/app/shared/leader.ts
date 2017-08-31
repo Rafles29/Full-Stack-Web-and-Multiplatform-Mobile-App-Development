bb53974a4c6ac7fae1cd63b7b2c11d876d1a9855
Rafles29 <rwozniak@intelgraf.com.pl>
2017-08-31 17:36:49 +0200

    2 week assigment Angular

---
+++ angular/conFusion/src/app/shared/leader.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by rwozn on 31.08.2017.
+ */
+export class Leader {
+  id: number;
+  name: string;
+  image: string;
+  designation: string;
+  abbr: string;
+  featured: boolean;
+  description: string;
+}
